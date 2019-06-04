7fe1cef47dbb090f9f127523daa57f572756418b
Nicolas B.Gagnon <nicolas.beaudoin-gagnon.1@ulaval.ca>
2019-06-04 19:49:43 -0400

    Centering cursor in middle of display

--- BalanceBoard.py
+++ BalanceBoard.py
@@ -96,7 +96,7 @@ class Cursor:
 
         dx, dy = self.gain * (self.angle_x_filtre - self.x), self.gain * (self.angle_y_filtre - self.y)
         self.x, self.y =  int(self.x + dx), int(self.y + dy)
-        print(self.x, self.y)
+        print(dx, dy)
 
         return self.x, self.y
 
