90a72dfc2293c0bcf0623a27cda2f170c8ff9371
Nicolas B.Gagnon <nicolas.beaudoin-gagnon.1@ulaval.ca>
2019-06-06 16:37:41 -0400

    Added new classes for level 2.

--- BalanceBoard.py
+++ BalanceBoard.py
@@ -1,9 +1,9 @@
 import pygame
-from pygame import gfxdraw
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
+from pygame import gfxdraw
+from colors import GREEN, ORANGE, RED, BLACK, WHITE
 from mpu6050_interface import MPU6050
 
 class Timer:
@@ -22,12 +22,13 @@ class Timer:
     def get_remaining_time(self):
         return (self.t_end - pygame.time.get_ticks()) // 1000 + 1
 
+
 class EmptyCircle:
-    def __init__(self, x, y, radius, color=(255, 255, 255)):
+    def __init__(self, x, y, radius):
         self.x = x
         self.y = y
-        self.radius = radius
-        self.color = color
+        self.R = radius
+        self.color = WHITE
 
     def cursor_is_inside(self, cursor):
         # Get mouse position.
@@ -36,21 +37,121 @@ class EmptyCircle:
         # Compute circle range.
         r = np.sqrt((cursor_x - self.x) ** 2 + (cursor_y - self.y) ** 2)
 
-        return r < self.radius
+        return r < self.R
 
     def draw(self, display):
         # Anti alisied circles (might break in futur pygame update)
-        # If so, just replace these 3 lines by:
-        # pygame.draw.circle(display, self.color, (self.x, self.y), self.radius, 2).
-        pygame.gfxdraw.aacircle(display, self.x, self.y, self.radius, self.color)
-        pygame.gfxdraw.aacircle(display, self.x, self.y, self.radius-1, self.color)
-        pygame.gfxdraw.aacircle(display, self.x, self.y, self.radius - 2, self.color)
+        # If so, just replace these 2 lines by:
+        #pygame.draw.circle(display, self.color, (self.x, self.y), self.R, 2)
+        pygame.gfxdraw.aacircle(display, self.x, self.y, self.R, self.color)
+        pygame.gfxdraw.aacircle(display, self.x, self.y, self.R-1, self.color)
 
     def update_radius(self, new_radius):
-        self.radius = new_radius
+        self.R = new_radius
+
+
+class FilledCircle(EmptyCircle):
+    def __init__(self, x, y, radius, color):
+        super().__init__(x, y, radius)
+        self.color = color
+
+    def draw(self, display):
+        pygame.gfxdraw.filled_circle(display, self.x, self.y, self.R, self.color)
+        pygame.gfxdraw.aacircle(display, self.x, self.y, self.R, WHITE)
+        pygame.gfxdraw.aacircle(display, self.x, self.y, self.R-1, WHITE)
+
+    def update_color(self, new_color):
+        self.color = new_color
+
+
+class Polygon:
+    def __init__(self, pointlist, color):
+        self.pointlist = pointlist
+        self.color = color
+
+    def cursor_is_inside(self, cursor):
+        # Get mouse position.
+        cursor_x, cursor_y = cursor.get_position()
+        return point_inside_polygon(cursor_x, cursor_y, self.pointlist)
+
+    def draw(self, display):
+        pygame.draw.polygon(display, self.color, self.pointlist)
+        pygame.gfxdraw.aapolygon(display,  self.pointlist, WHITE)
+
+    def update_color(self, new_color):
+        self.color = new_color
+
+
+class Course:
+    def __init__(self, angle_in_degrees, length,
+                 start_circle_r, end_circle_r, duration_in_seconds=3,
+                 start_circle_color=ORANGE, end_circle_color=ORANGE,
+                 course_color=ORANGE):
+
+        self.x_center, self.y_center = get_center_of_display()
+        self.length = length
+        self.timer = Timer(duration_in_seconds)
+        self.angle_in_rads = np.radians(-angle_in_degrees)
+
+        # colors
+        self.course_color = course_color
+        self.start_circle_color = start_circle_color
+        self.end_circle_color = end_circle_color
+
+        x_end = int(self.x_center + length * np.cos(self.angle_in_rads))
+        y_end = int(self.y_center + length * np.sin(self.angle_in_rads))
+
+        self.start_circle = FilledCircle(self.x_center, self.y_center, start_circle_r, self.start_circle_color)
+        self.end_circle = FilledCircle(x_end, y_end, end_circle_r, self.end_circle_color)
+        self.polygon = Polygon(self._compute_polygon_points(self.x_center, self.y_center), self.course_color)
+
+    def _compute_polygon_points(self, x_center, y_center):
+        points = []
+
+        sin = np.sin(self.angle_in_rads)
+        cos = np.cos(self.angle_in_rads)
+        sin0 = np.sin(self.angle_in_rads - np.pi/2)
+        cos0 = np.cos(self.angle_in_rads - np.pi/2)
+
+        p0_x = int(x_center + self.start_circle.R * cos0)
+        p0_y = int(y_center + self.start_circle.R * sin0)
+        points.append((p0_x, p0_y))
+
+        p1_x = int(x_center - self.start_circle.R * cos0)
+        p1_y = int(y_center - self.start_circle.R * sin0)
+        points.append((p1_x, p1_y))
+
+        p2_x = int(x_center + self.length * cos - self.end_circle.R * sin)
+        p2_y = int(y_center + self.length * sin + self.end_circle.R * cos)
+        points.append((p2_x, p2_y))
+
+        p3_x = int(x_center + self.length * cos + self.end_circle.R * sin)
+        p3_y = int(y_center + self.length * sin - self.end_circle.R * cos)
+        points.append((p3_x, p3_y))
+        return points
+
+    def cursor_inside_end_circle(self, cursor):
+        return self.end_circle.cursor_is_inside(cursor)
+
+    def cursor_inside_start_circle(self, cursor):
+        return self.start_circle.cursor_is_inside(cursor)
+
+    def cursor_inside_trail(self, cursor):
+        return self.polygon.cursor_is_inside(cursor)
+
+    def draw(self, display):
+        self.polygon.draw(display)
+        self.start_circle.draw(display)
+        self.end_circle.draw(display)
+
+    def update_colors(self, start_circle_color, course_color, end_circle_color):
+        self.start_circle.update_color(start_circle_color)
+        self.polygon.update_color(course_color)
+        self.end_circle.update_color(end_circle_color)
+
 
 class Text:
-    def __init__(self, text, x, y, color=(255, 255, 255)):
+    def __init__(self, text, x, y, color=WHITE):
         self.x = x
         self.y = y
         self.color = color
@@ -68,14 +169,16 @@ class Text:
     def hide(self,):
         self.change_text("")
 
+
 class Cursor:
-    def __init__(self, big_circle_r, x_center, y_center, cursor_r, color=(0, 0, 0)):
+    def __init__(self, big_circle_r, cursor_r, color=BLACK):
+
+        self.x_center, self.y_center = get_center_of_display()
+        self.x = self.x_center
+        self.y = self.y_center
+        self.cursor_r = cursor_r
         self.color = color
-        self.x = x_center
-        self.y = y_center
-        
-        self.x_center = x_center
-        self.y_center = y_center
+
         self.gain = 6*big_circle_r / 90
         self.mpu6050 = MPU6050()
 
@@ -84,8 +187,8 @@ class Cursor:
         self.angle_y_filtre = y_rotation
         self.gyro_offset_x = x_gyro
         self.gyro_offset_y = y_gyro
-        self.gyro_total_x = (self.angle_x_filtre) - self.gyro_offset_x
-        self.gyro_total_y = (self.angle_y_filtre) - self.gyro_offset_y
+        self.gyro_total_x = self.angle_x_filtre - self.gyro_offset_x
+        self.gyro_total_y = self.angle_y_filtre - self.gyro_offset_y
 
         self.dt = 0.01
         self.K = 0.945
@@ -110,11 +213,25 @@ class Cursor:
         self.y = int((self.y + self.y_center + self.gain * self.angle_y_filtre)//2)
         
     def draw(self, display):
-        pygame.draw.circle(display, self.color, self.get_position(), 6)
+        pygame.draw.circle(display, self.color, self.get_position(), self.cursor_r)
         
     def get_position(self,):
         self.update_position()
         return self.x, self.y
+
+
+class Mouse:
+    def __init__(self, _, cursor_r, color=BLACK):
+        self.color = color
+        self.cursor_r = cursor_r
+
+    def draw(self, display):
+        pygame.draw.circle(display, self.color, self.get_position(), self.cursor_r)
+
+    def get_position(self,):
+        x, y = pygame.mouse.get_pos()
+        return x, y
+
 
 class DataFile:
     def __init__(self, path_to_csv_file):
@@ -142,7 +259,10 @@ class DataFile:
                                                     cursor.gyro_total_y,
                                                     cursor.angle_x_filtre,
                                                     cursor.angle_y_filtre))
-        
+
+def get_center_of_display():
+    x, y = pygame.display.get_surface().get_size()
+    return x//2, y//2
 
 def plot_session_graphs(path_to_file):
     fichierData = open(path_to_file, 'r')
@@ -219,3 +339,44 @@ def display_congrats(display, bg_color, text_color):
     pygame.display.update()
     pygame.time.delay(4000)
 
+
+def point_inside_polygon(x, y, poly, include_edges=True):
+    '''
+    Test if point (x,y) is inside polygon poly.
+
+    poly is N-vertices polygon defined as
+    [(x1,y1),...,(xN,yN)] or [(x1,y1),...,(xN,yN),(x1,y1)]
+    (function works fine in both cases)
+
+    Geometrical idea: point is inside polygon if horisontal beam
+    to the right from point crosses polygon even number of times.
+    Works fine for non-convex polygons.
+    '''
+    n = len(poly)
+    inside = False
+
+    p1x, p1y = poly[0]
+    for i in range(1, n + 1):
+        p2x, p2y = poly[i % n]
+        if p1y == p2y:
+            if y == p1y:
+                if min(p1x, p2x) <= x <= max(p1x, p2x):
+                    # point is on horisontal edge
+                    inside = include_edges
+                    break
+                elif x < min(p1x, p2x):  # point is to the left from current edge
+                    inside = not inside
+        else:  # p1y!= p2y
+            if min(p1y, p2y) <= y <= max(p1y, p2y):
+                xinters = (y - p1y) * (p2x - p1x) / float(p2y - p1y) + p1x
+
+                if x == xinters:  # point is right on the edge
+                    inside = include_edges
+                    break
+
+                if x < xinters:  # point is to the left from current edge
+                    inside = not inside
+
+        p1x, p1y = p2x, p2y
+
+    return inside
