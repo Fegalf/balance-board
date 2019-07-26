5f2bca84c577f3ef27fc1b5920520a683abd1dc6
Nicolas B.Gagnon <nicolas.beaudoin-gagnon.1@ulaval.ca>
2019-07-25 20:45:49 -0400

    adfadfgadf

--- BalanceBoard.py
+++ BalanceBoard.py
@@ -317,7 +317,11 @@ class DataFile:
         date = datetime.date.today().strftime("%Y%m%d")
         time = datetime.datetime.today().strftime("%H%M%S")
         
-        self.path_to_csv_file = path_to_data_folder + f"/{participant_id}_jeu{game_id}_{date}_{time[:2]}h_{time[2:4]}m_{time[4:]}s.csv"
+        self.path_to_csv_file = path_to_data_folder + r"/{}_jeu{}_{}_{}h_{}m_{}s.csv".format(participant_id,
+                                                                                            game_id,
+                                                                                            date,
+                                                                                            time[:2],
+                                                                                            time[2:4])
         self.f = open(self.path_to_csv_file, 'w')
         self.f.write("{0:}, {1:}, {2:}, {3:}, {4:}, "
                      "{5:}, {6:}, {7:}\n".format('time', 'niveau',
